c54cdb42df78f620c37d4b1c7071fde0ddcc97a0
Joachim Breitner <mail@joachim-breitner.de>
2012-11-30 14:52:47 +0000

    Need to loosen NoBH semantics to show one direction (probably making the other direction harder)

--- LaunchburyNoBH.thy
+++ LaunchburyNoBH.thy
@@ -14,13 +14,15 @@ where
       (x, App e y) # \<Gamma> [\<Down>] \<Theta>" 
  | Variable: "\<lbrakk>
       (y, e) \<in> set ((x, Var y) # \<Gamma>);
-      (y, e) # delete y ((x, Var y) # \<Gamma>) [\<Down>] (y, z) # \<Delta>
+      (y, e) # delete y ((x, Var y) # \<Gamma>) [\<Down>] (y, z) # \<Delta>;
+      set \<Delta>' = set ((y,z) # \<Delta>)
    \<rbrakk> \<Longrightarrow>
-      (x, Var y) # \<Gamma> [\<Down>] (x,z) # delete x ((y,z) # \<Delta>)"
+      (x, Var y) # \<Gamma> [\<Down>] (x,z) # delete x \<Delta>'"
  | Let: "\<lbrakk>
       set (bn as) \<sharp>* (\<Gamma>, x, Let as body);
       distinctVars (asToHeap as);
-      (x, body) # asToHeap as @ \<Gamma> [\<Down>] \<Delta>
+      set \<Gamma>' = set (asToHeap as @ \<Gamma>);
+      (x, body) # \<Gamma>' [\<Down>] \<Delta>
    \<rbrakk> \<Longrightarrow>
       (x, Let as body) # \<Gamma> [\<Down>] \<Delta>"
 
@@ -50,17 +52,19 @@ lemma eval_test2:
   apply (rule Let)
     apply (simp add: fresh_Pair fresh_Cons fresh_at_base  fresh_Nil exp_assn.fresh fresh_star_def exp_assn.bn_defs)
     apply simp
+    apply (rule refl)
   apply (rule obtain_fresh[where x = "(asToHeap (ACons y (Lam [z]. Var z) ANil) @ [], [(y, Lam [z]. Var z)], x, Var y, y,
                         [(x, Lam [z]. Var z), (y, Lam [z]. Var z)], z)", standard])
   apply (rule Application[where z = z])
     apply (auto simp add: fresh_Pair fresh_Cons fresh_Nil exp_assn.fresh)[1]
     defer
     defer
-    apply (rule Variable)
+    apply (rule VariableI)
     apply auto[1]
    
     apply (auto simp add: fresh_Pair fresh_Cons fresh_Nil exp_assn.fresh fresh_at_base)[1]
     apply (rule Lambda)
+    apply (auto simp add: fresh_Pair fresh_Cons fresh_Nil exp_assn.fresh fresh_at_base)[1]
     apply (auto simp add: fresh_Pair fresh_Cons fresh_Nil exp_assn.fresh fresh_at_base)[1]
     apply (rule VariableI)
     apply simp
@@ -69,5 +73,35 @@ lemma eval_test2:
     apply (auto simp add: fresh_Pair fresh_Cons fresh_Nil exp_assn.fresh fresh_at_base)[1]
     apply (auto simp add: fresh_Pair fresh_Cons fresh_Nil exp_assn.fresh fresh_at_base)[1]
   done
+
+inductive distinct_reds :: "heap \<Rightarrow> heap \<Rightarrow> bool" ("_ [\<Down>]d _" [50,50] 50)
+where
+  DLambda: "(x, (Lam [y]. e)) # \<Gamma> [\<Down>]d (x, (Lam [y]. e)) # \<Gamma>" 
+ | DApplication: "\<lbrakk>
+      atom n \<sharp> (\<Gamma>,delete x \<Delta>,x,e,y,\<Theta>,z);
+      atom z \<sharp> (\<Gamma>,delete x \<Delta>,x,e,y,\<Theta>);
+
+      (n, e) # (x, App (Var n) y) # \<Gamma> [\<Down>]d (n, Lam [z]. e') # \<Delta>;
+      (x, e'[z ::= y]) # delete x \<Delta> [\<Down>]d \<Theta>
+    \<rbrakk> \<Longrightarrow>
+      (x, App e y) # \<Gamma> [\<Down>]d \<Theta>" 
+ | DVariable: "\<lbrakk>
+      (y, e) \<in> set ((x, Var y) # \<Gamma>);
+      (y, e) # delete y ((x, Var y) # \<Gamma>) [\<Down>]d (y, z) # \<Delta>
+   \<rbrakk> \<Longrightarrow>
+      (x, Var y) # \<Gamma> [\<Down>]d (x,z) # delete x ((y,z) # \<Delta>)"
+ | DLet: "\<lbrakk>
+      set (bn as) \<sharp>* (\<Gamma>, x, Let as body);
+      distinctVars (asToHeap as);
+      (x, body) # asToHeap as @ \<Gamma> [\<Down>]d \<Delta>
+   \<rbrakk> \<Longrightarrow>
+      (x, Let as body) # \<Gamma> [\<Down>]d \<Delta>"
+
+equivariance distinct_reds
+
+nominal_inductive distinct_reds
+  avoids DApplication: "n" and "z"
+  by (auto simp add: fresh_star_def fresh_Cons fresh_Pair exp_assn.fresh)
+
 
 end
--- LaunchburyUnBH.thy
+++ LaunchburyUnBH.thy
@@ -41,42 +41,43 @@ case (Application n \<Gamma> \<Gamma>' \<Delta> \<Delta>' x e y \<Theta> \<Theta
   qed
 next
 case (Variable y e \<Gamma> x \<Gamma>' z \<Delta>' \<Delta>)
-  have "((x, Var y) # \<Gamma>') @ \<Gamma> [\<Down>] (x, z) # (y, z) # \<Delta>' @ \<Delta>"
-  unfolding append_Cons
-  proof (rule LaunchburyNoBH.VariableI)
+  have [simp]:"x \<noteq> y"
+    using Variable(3)
+    by (auto simp add: distinctVars_Cons)
+  note this[symmetric,simp]
+
+  have "y \<notin> heapVars \<Gamma>'"
+    using Variable(3)
+    by (auto simp add: distinctVars_Cons)
+  hence [simp]: "delete y \<Gamma>' = \<Gamma>'"
+    by (rule delete_no_there)
+
+  have "x \<notin> heapVars \<Delta>'"
+    using Variable(4)
+    by (auto simp add: distinctVars_Cons)
+  hence [simp]: "delete x \<Delta>' = \<Delta>'"
+    by (rule delete_no_there)
+
+  have "x \<notin> heapVars \<Delta>"
+    using Variable(4)
+    by (auto simp add: distinctVars_Cons)
+  hence [simp]: "delete x \<Delta> = \<Delta>"
+    by (rule delete_no_there)
+
+  have "((x, Var y) # \<Gamma>') @ \<Gamma> [\<Down>] (x, z) # delete x (((x, Var y) # \<Delta>') @ (y, z) # \<Delta>)"
+  unfolding append_Cons 
+  proof (rule LaunchburyNoBH.Variable)
     show "(y, e) \<in> set ((x, Var y) # \<Gamma>' @ \<Gamma>)"
       using Variable(1) by simp
-    have [simp]:"x \<noteq> y"
-      using Variable(3)
-      by (auto simp add: distinctVars_Cons)
-    note this[symmetric,simp]
-
-    have "y \<notin> heapVars \<Gamma>'"
-      using Variable(3)
-      by (auto simp add: distinctVars_Cons)
-    hence [simp]: "delete y \<Gamma>' = \<Gamma>'"
-      by (rule delete_no_there)
-
-    have "x \<notin> heapVars \<Delta>'"
-      using Variable(4)
-      by (auto simp add: distinctVars_Cons)
-    hence [simp]: "delete x \<Delta>' = \<Delta>'"
-      by (rule delete_no_there)
-
-    have "x \<notin> heapVars \<Delta>"
-      using Variable(4)
-      by (auto simp add: distinctVars_Cons)
-    hence [simp]: "delete x \<Delta> = \<Delta>"
-      by (rule delete_no_there)
-
     show "(y, e) # delete y ((x, Var y) # \<Gamma>' @ \<Gamma>) [\<Down>] (y, z) # (((x, Var y) # \<Delta>') @ \<Delta>)"
       using Variable(7) by simp
    
-    show "(y, z) # \<Delta>' @ \<Delta> = delete x ((y, z) # ((x, Var y) # \<Delta>') @ \<Delta>)"
-      by simp
+    show "set ((x, Var y) # \<Delta>' @ (y, z) # \<Delta>) = set ((y, z) # ((x, Var y) # \<Delta>') @ \<Delta>)"
+      by auto
   qed
   thus ?case
-    sorry (* Reordering lemma *)
+    unfolding append_Cons
+    by simp
 next
 case (Let as \<Gamma> x body \<Gamma>' \<Delta>' \<Delta>)
   show ?case
@@ -85,10 +86,10 @@ case (Let as \<Gamma> x body \<Gamma>' \<Delta>' \<Delta>)
     show "set (bn as) \<sharp>* (\<Gamma>' @ \<Gamma>, x, Terms.Let as body)"
       using Let(1) by (simp add: fresh_star_Pair fresh_star_append)
     show "distinctVars (asToHeap as)" by fact
-    show " (x, body) # asToHeap as @ \<Gamma>' @ \<Gamma> [\<Down>] \<Delta>' @ \<Delta>"
-      using Let(7)
-      unfolding append_Cons
-      sorry (* reordering-lemma *)
+    show " (x, body) # \<Gamma>' @ asToHeap as  @ \<Gamma> [\<Down>] \<Delta>' @ \<Delta>"
+      using Let(7) by simp
+    show "set (\<Gamma>' @ asToHeap as @ \<Gamma>) = set (asToHeap as @ \<Gamma>' @ \<Gamma>)"
+      by auto
 qed
 
 
