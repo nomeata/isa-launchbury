37691afad9a8a8acea2ea4fc0795769ff089cdb4
Joachim Breitner <mail@joachim-breitner.de>
2013-06-14 11:02:33 +0000

    Ensure that only defined values are on the stack

--- Launchbury/LaunchburyCombinedTagged.thy
+++ Launchbury/LaunchburyCombinedTagged.thy
@@ -101,6 +101,10 @@ lemma eval_test2:
 
 subsubsection {* Properties of the semantics *}
 
+lemma stack_head_there:
+  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> hd S \<in> heapVars \<Gamma>"
+  by (induct rule: reds.induct) (auto simp add: perm_heapVars)
+
 text {*
 This is the same semantics with additional distinctiveness requirements. This is defined in order to
 obtain a more convenient induction rule.
@@ -110,7 +114,8 @@ inductive distinct_reds :: "heap \<Rightarrow> Flag \<Rightarrow> Flag \<Rightar
 where
   DLambda: "\<lbrakk>
       distinctVars ((x, (Lam [y]. e)) # \<Gamma>);
-      distinct (x#S)
+      distinct (x#S);
+      set S \<subseteq> heapVars \<Gamma>
   \<rbrakk> \<Longrightarrow> 
       (x, (Lam [y]. e)) # \<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>x#S\<^esub> (x, (Lam [y]. e)) # \<Gamma>" 
  | DApplication: "\<lbrakk>
@@ -123,6 +128,8 @@ where
       distinctVars \<Theta>;
       distinct (x#S);
       distinct (n#x#S);
+      set S \<subseteq> heapVars \<Gamma>;
+      set S \<subseteq> heapVars \<Delta>;
       (n, e) # (x, App (Var n) y) # \<Gamma> \<Down>\<^sup>\<times>\<^sup>u\<^sup>d\<^bsub>n#x#S\<^esub> (n, Lam [z]. e') # (x, App (Var n) y) # \<Delta>;
       (x, e'[z ::= y]) # \<Delta> \<Down>\<^sup>\<times>\<^sup>u\<^sup>d\<^bsub>x#S\<^esub> \<Theta>
     \<rbrakk> \<Longrightarrow>
@@ -137,6 +144,8 @@ where
       distinctVars \<Theta>;
       distinct (x#S);
       distinct (n#x#S);
+      set S \<subseteq> heapVars \<Gamma>;
+      set S \<subseteq> heapVars \<Delta>;
       (n, e) # (x, App (Var n) y) # \<Gamma> \<Down>\<^sup>\<surd>\<^sup>u\<^sup>d\<^bsub>n#x#S\<^esub> (n, Lam [z]. e') # (x, App (Var n) y) # \<Delta>;
       (z,Var y) # (x, e') # \<Delta> \<Down>\<^sup>\<surd>\<^sup>u\<^sup>d\<^bsub>x#S\<^esub> \<Theta>
     \<rbrakk> \<Longrightarrow>
@@ -148,6 +157,8 @@ where
       distinctVars ((y,z) # (x, z) # \<Delta>);
       distinct (x#S);
       distinct (y#x#S);
+      set S \<subseteq> heapVars \<Gamma>;
+      set S \<subseteq> heapVars \<Delta>;
       (x, Var y) # \<Gamma> \<Down>\<^sup>i\<^sup>\<surd>\<^sup>d\<^bsub>y#x#S\<^esub> (y,z) # (x, Var y) # \<Delta>
    \<rbrakk> \<Longrightarrow>
       (x, Var y) # \<Gamma> \<Down>\<^sup>i\<^sup>\<surd>\<^sup>d\<^bsub>x#S\<^esub> (y,z) # (x,z) # \<Delta>"
@@ -157,6 +168,8 @@ where
       distinctVars \<Delta>;
       distinct (x#S);
       distinct (y#x#S);
+      set S \<subseteq> heapVars \<Gamma>;
+      set S \<subseteq> heapVars \<Delta>;
       (x, e) # (y,e) # \<Gamma> \<Down>\<^sup>i\<^sup>\<times>\<^sup>d\<^bsub>x#y#S\<^esub> \<Delta>
    \<rbrakk> \<Longrightarrow>
       (x, Var y) # (y,e) # \<Gamma> \<Down>\<^sup>i\<^sup>\<times>\<^sup>d\<^bsub>x#S\<^esub> \<Delta>"
@@ -167,6 +180,8 @@ where
       distinctVars ((x, body) # asToHeap as);
       distinctVars \<Delta>;
       distinct (x#S);
+      set S \<subseteq> heapVars \<Gamma>;
+      set S \<subseteq> heapVars \<Delta>;
       (x, body) # asToHeap as @ \<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>x#S\<^esub> \<Delta>
    \<rbrakk> \<Longrightarrow>
       (x, Let as body) # \<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>x#S\<^esub> \<Delta>"
@@ -178,6 +193,10 @@ where
       distinctVars \<Delta>;
       distinctVars \<Delta>';
       distinct S;
+      set S \<subseteq> heapVars \<Gamma>;
+      set S \<subseteq> heapVars \<Delta>;
+      set S \<subseteq> heapVars \<Gamma>';
+      set S \<subseteq> heapVars \<Delta>';
       \<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta>
    \<rbrakk> \<Longrightarrow> 
       \<Gamma>' \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta>'"
@@ -200,10 +219,16 @@ lemma distinct_redsD3:
   "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> distinctVars \<Delta>"
   by (induct rule: distinct_reds.induct) simp_all
 
-  thm DApplication[no_vars]
+lemma distinct_redsD4:
+  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> set S \<subseteq> heapVars \<Gamma>"
+  by (induct rule: distinct_reds.induct) auto
+
+lemma distinct_redsD5:
+  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> set S \<subseteq> heapVars \<Delta>"
+  by (induct rule: distinct_reds.induct) auto
 
 lemma distinct_redsI:
-  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> distinctVars \<Gamma> \<Longrightarrow> distinct S \<Longrightarrow> \<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta>"
+  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> distinctVars \<Gamma> \<Longrightarrow> distinct S \<Longrightarrow> set S \<subseteq> heapVars \<Gamma> \<Longrightarrow> \<Gamma> \<Down>\<^sup>i\<^sup>u\<^sup>d\<^bsub>S\<^esub> \<Delta>"
 proof (nominal_induct rule: reds.strong_induct)
 case Lambda thus ?case by (auto intro: DLambda)
 next
@@ -213,20 +238,24 @@ case (Application n \<Gamma> x e y S \<Delta> \<Theta> z u e')
   moreover
 
   from Application
-  have "distinctVars ((n, e) # (x, App (Var n) y) # \<Gamma>)" and "distinct (n#x#S)"
+  have "distinctVars ((n, e) # (x, App (Var n) y) # \<Gamma>)" and
+       "distinct (n#x#S)" and 
+        "set (n # x # S) \<subseteq> heapVars ((n, e) # (x, App (Var n) y) # \<Gamma>)"
     by (auto simp add: distinctVars_Cons heapVars_not_fresh set_not_fresh)
   note Application(17)[OF this]
   moreover
 
-  from distinct_redsD3[OF this]
-  have "distinctVars ((x, e'[z::=y]) # \<Delta>)"
+  from distinct_redsD3[OF this]  distinct_redsD5[OF this] `atom n \<sharp> S`
+  have "distinctVars ((x, e'[z::=y]) # \<Delta>)" and "set (x # S) \<subseteq> heapVars ((x, e'[z::=y]) # \<Delta>)"
     by (auto simp add: distinctVars_Cons heapVars_not_fresh set_not_fresh)
-  note Application(19)[OF this  `distinct (x # S)`]
+  note Application(19)[OF this(1)  `distinct (x # S)` this(2)]
   moreover
   note `distinctVars ((x, App e y) # \<Gamma>)` `distinct (x#S)` `distinct (n#x#S)`
+  moreover
+  note `set (x # S) \<subseteq> heapVars ((x, e'[z::=y]) # \<Delta>)` `set (n # x # S) \<subseteq> heapVars ((n, e) # (x, App (Var n) y) # \<Gamma>)`
   ultimately
   show ?case
-    by (auto intro: DApplication elim:  distinct_redsD2 distinct_redsD3)
+    by (auto intro: DApplication elim: distinct_redsD2 distinct_redsD3)
 next
 case (ApplicationInd n \<Gamma> x e y S \<Delta> \<Theta> z u e')
   have "atom n \<sharp> (\<Gamma>, x, e, y, S, \<Delta>, \<Theta>, z)" and "atom z \<sharp> (\<Gamma>, x, e, y, S, \<Delta>)"
@@ -234,54 +263,64 @@ case (ApplicationInd n \<Gamma> x e y S \<Delta> \<Theta> z u e')
   moreover
 
   from ApplicationInd
-  have "distinctVars ((n, e) # (x, App (Var n) y) # \<Gamma>)" and "distinct (n#x#S)"
+  have "distinctVars ((n, e) # (x, App (Var n) y) # \<Gamma>)"
+      and "distinct (n#x#S)"
+      and "set (n # x # S) \<subseteq> heapVars ((n, e) # (x, App (Var n) y) # \<Gamma>)"
     by (auto simp add: distinctVars_Cons heapVars_not_fresh set_not_fresh)
   note ApplicationInd(16)[OF this]
   moreover
 
-  from distinct_redsD3[OF this] `atom z \<sharp> x` `atom z \<sharp> \<Delta>`
-  have "distinctVars ((z, Var y) # (x, e') # \<Delta>)"
+  from distinct_redsD3[OF this] `atom z \<sharp> x` `atom z \<sharp> \<Delta>` distinct_redsD5[OF this] `atom n \<sharp> S`
+  have "distinctVars ((z, Var y) # (x, e') # \<Delta>)" "set (x # S) \<subseteq> heapVars ((z, Var y) # (x, e') # \<Delta>)"
     by (auto simp add: distinctVars_Cons heapVars_not_fresh set_not_fresh)
-  note ApplicationInd(18)[OF this  `distinct (x # S)`]
+  note ApplicationInd(18)[OF this(1) `distinct (x # S)` this(2)]
   moreover
   note `distinctVars ((x, App e y) # \<Gamma>)` `distinct (x#S)` `distinct (n#x#S)`
+  moreover
+  note `set (n # x # S) \<subseteq> heapVars ((n, e) # (x, App (Var n) y) # \<Gamma>)`
+       `set (x # S) \<subseteq> heapVars ((z, Var y) # (x, e') # \<Delta>)`
   ultimately
   show ?case
-    by (auto intro: DApplicationInd elim:  distinct_redsD2 distinct_redsD3)
+    apply (auto intro!: DApplicationInd elim:  distinct_redsD2 distinct_redsD3)
+    by (metis  `atom z \<sharp> S` set_mp set_not_fresh subset_insert)
 next
 case (Variable y x S \<Gamma> i z \<Delta>)
-  hence "distinct (y # x # S)" by auto
-  from Variable(3)[OF `distinctVars ((x, Var y) # \<Gamma>)` this]
+  with stack_head_there[OF Variable(2)]
+  have "distinct (y # x # S)" and "set (y # x # S) \<subseteq> heapVars ((x, Var y) # \<Gamma>)" by auto
+  from Variable(3)[OF `distinctVars ((x, Var y) # \<Gamma>)` this ]
+        distinct_redsD5[OF Variable(3)[OF `distinctVars ((x, Var y) # \<Gamma>)` this ]]
         `distinctVars ((x, Var y) # \<Gamma>)`
-        `distinct (x#S)` `y \<notin> set (x # S)`
+        `distinct (x#S)` `y \<notin> set (x # S)` `set (x # S) \<subseteq> heapVars ((x, Var y) # \<Gamma>)`
   show ?case
     by (auto intro!: DVariable dest: distinct_redsD3 simp add: distinctVars_Cons)
 next
 case (VariableNoUpd y x S e \<Gamma> i \<Delta>)
-  thus ?case by (auto intro: DVariableNoUpd elim: distinct_redsD3 simp add: distinctVars_Cons)
+  with stack_head_there[OF VariableNoUpd(2)]
+  show ?case
+    by (auto intro!: DVariableNoUpd elim: distinct_redsD3  set_mp[OF distinct_redsD5] simp add: distinctVars_Cons)
 next
 case (Let as \<Gamma> x body i u S \<Delta>)
   moreover
-  from Let(1-4,6)
-  have "distinctVars ((x, body) # asToHeap as @ \<Gamma>)"
+  from Let(1-4,6,8)
+  have "distinctVars ((x, body) # asToHeap as @ \<Gamma>)" and "set (x # S) \<subseteq> heapVars ((x, body) # asToHeap as @ \<Gamma>)"
     apply (auto simp add: distinctVars_Cons distinctVars_append dest: fresh_assn_distinct)
     by (metis (hide_lams, mono_tags) IntI all_not_in_conv fresh_assn_distinct fresh_star_Pair fresh_star_list(1) fresh_star_list(2)  heapVars_from_set in_set_conv_decomp let_binders_fresh)
   moreover
-  note Let(5)[OF this `distinct (x # S)`]
+  note Let(5)[OF this(1) `distinct (x # S)` this(2)]
   ultimately
   show ?case
-    by (auto intro!: DLet dest: distinct_redsD3 simp add: distinctVars_Cons fresh_star_Pair)
+    by (auto intro!: DLet dest: distinct_redsD3  set_mp[OF distinct_redsD5] simp add: distinctVars_Cons fresh_star_Pair)
 next
 case (Permute \<Gamma> \<Gamma>' \<Delta> \<Delta>' i u S)
   thus ?case
     apply -
     apply (rule DPermute, assumption+)
-    apply (auto dest: distinct_redsD3 simp add: perm_distinctVars)
+    apply (auto dest: distinct_redsD3 set_mp[OF distinct_redsD5] simp add: perm_distinctVars perm_heapVars)
     done
 qed
 
 lemma reds_pres_distinctVars:
-  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> distinctVars \<Gamma> \<Longrightarrow> distinct S \<Longrightarrow> distinctVars \<Delta>"
+  "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^bsub>S\<^esub> \<Delta> \<Longrightarrow> distinctVars \<Gamma> \<Longrightarrow> distinct S \<Longrightarrow> set S \<subseteq> heapVars \<Gamma> \<Longrightarrow> distinctVars \<Delta>"
 by (metis distinct_redsD3 distinct_redsI)
 
 lemmas reds_distinct_ind = distinct_reds.induct[OF distinct_redsI, consumes 2, case_names Lambda Application Variable Let]
@@ -300,6 +339,7 @@ lemma reds_doesnt_forget:
  assumes "\<Gamma> \<Down>\<^sup>i\<^sup>u\<^bsub>S\<^esub> \<Delta>"
  assumes "distinctVars \<Gamma>"
  assumes "distinct S"
+ assumes "set S \<subseteq> heapVars \<Gamma>"
  shows "heapVars \<Gamma> \<subseteq> heapVars \<Delta>"
 by (rule reds_doesnt_forget'[OF distinct_redsI[OF assms]])+
 
@@ -319,8 +359,10 @@ lemma stack_unchanged:
   assumes "(x,e) \<in> set \<Gamma>"
   shows   "(x,e) \<in> set \<Delta>"
 using assms
- by (induct arbitrary: x e rule:distinct_reds.induct)(auto simp add: perm_set_eq)
-
+ apply (induct \<Gamma> i u S \<Delta> arbitrary: x e rule:distinct_reds.induct)
+ apply (auto simp add: perm_set_eq)
+ apply metis+
+ done
 
 text {* 
 Fresh variables either stay fresh or are added to the heap.
@@ -336,20 +378,20 @@ case (DApplication n \<Gamma> x e y S \<Delta> \<Theta> z e' u x')
   from `atom n \<sharp> x'` `atom x' \<sharp> (x, App e y) # \<Gamma>`
   have "atom x' \<sharp> (n, e) # (x, App (Var n) y) # \<Gamma>"
     by (auto simp add: fresh_Pair fresh_Cons)
-  from DApplication.hyps(26)[OF this]
+  from DApplication.hyps(28)[OF this]
   show ?case
   proof
     assume "atom x' \<sharp> (n, Lam [z]. e') # (x, App (Var n) y) # \<Delta>"
     with `atom z \<sharp> x'`
     have "atom x' \<sharp> (x, e'[z::=y]) # \<Delta>"
       by (simp add: fresh_Cons fresh_Pair subst_pres_fresh fresh_at_base)
-    from DApplication.hyps(28)[OF this]
+    from DApplication.hyps(30)[OF this]
     show ?thesis.
   next
     assume "x' \<in> heapVars ((n, Lam [z]. e') # (x, App (Var n) y) # \<Delta>)"
     with `atom n \<sharp> x'`
     have "x' \<in> heapVars ((x, e'[z::=y]) # \<Delta>)" by (simp add: fresh_at_base)
-    with reds_doesnt_forget'[OF DApplication(27)]
+    with reds_doesnt_forget'[OF DApplication(29)]
     have "x' \<in> heapVars \<Theta>" by auto
     thus ?thesis..
   qed
@@ -358,27 +400,27 @@ case (DApplicationInd n \<Gamma> x e y S \<Delta> \<Theta> z e' u x')
   show ?case
   proof(cases "x' = z")
   case True
-    with  reds_doesnt_forget'[OF DApplicationInd(25)]
+    with  reds_doesnt_forget'[OF DApplicationInd(27)]
     have "x' \<in> heapVars \<Theta>" by auto
     thus ?thesis..
   next
   case False
     hence "atom x' \<sharp> (n, e) # (x, App (Var n) y) # \<Gamma>"
       using DApplicationInd by (auto simp add: fresh_Pair fresh_Cons)
-    from DApplicationInd.hyps(24)[OF this]
+    from DApplicationInd.hyps(26)[OF this]
     show ?thesis
     proof
       assume "atom x' \<sharp> (n, Lam [z]. e') # (x, App (Var n) y) # \<Delta>"
       with `atom n \<sharp> x'` `x' \<noteq> z`
       have "atom x' \<sharp> (z, Var y) # (x, e') # \<Delta>"
         by (simp add: fresh_Cons fresh_Pair subst_pres_fresh fresh_at_base)
-      from DApplicationInd.hyps(26)[OF this]
+      from DApplicationInd.hyps(28)[OF this]
       show ?thesis.
     next
       assume "x' \<in> heapVars ((n, Lam [z]. e') # (x, App (Var n) y) # \<Delta>)"
       with `atom n \<sharp> x'`
       have "x' \<in> heapVars ((z,Var y) # (x, e') # \<Delta>)" by (simp add: fresh_at_base)
-      with reds_doesnt_forget'[OF DApplicationInd(25)]
+      with reds_doesnt_forget'[OF DApplicationInd(27)]
       have "x' \<in> heapVars \<Theta>" by auto
       thus ?thesis..
     qed
@@ -394,7 +436,7 @@ case (DLet as \<Gamma> x body \<Delta> S i u x')
   show ?case
   proof(cases "x' \<in> heapVars (asToHeap as)")
     case True
-    with reds_doesnt_forget'[OF DLet(8)]
+    with reds_doesnt_forget'[OF DLet(10)]
     show ?thesis by auto
   next
     case False
@@ -402,7 +444,7 @@ case (DLet as \<Gamma> x body \<Delta> S i u x')
     with `atom x' \<sharp> (x, Let as body) # \<Gamma>`
     have "atom x' \<sharp> (x, body) # asToHeap as @ \<Gamma>"
       by (auto simp add: fresh_Cons fresh_Pair fresh_append fresh_fun_eqvt_app asToHeap_eqvt)
-    from DLet(9)[OF this]
+    from DLet(11)[OF this]
     show ?thesis.
   qed
 next 
