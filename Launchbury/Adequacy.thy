1f8508ce20ce1365923e03bb5dcc4d78666ea4b0
Joachim Breitner <mail@joachim-breitner.de>
2013-10-02 12:19:44 +0000

    Adequacy attempts

---
+++ Launchbury/Adequacy.thy
@@ -0,0 +1,104 @@
+theory Adequacy
+imports "Resourced-Denotational-Props" "Launchbury" "DistinctVars"
+begin
+
+lemma VariableNoBH:
+  assumes "(x, e) \<in> set \<Gamma>"
+  assumes "\<Gamma> : e \<Down>\<^bsub>x # L\<^esub> \<Delta> : z"
+  shows "\<Gamma> : Var x \<Down>\<^bsub>L\<^esub> (x, z) # delete x \<Delta> : z"
+sorry
+  
+
+theorem adequacy:
+  assumes "(\<N>\<lbrakk>e\<rbrakk>\<^bsub>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>"
+  shows "\<exists> \<Delta> v. \<Gamma> : e \<Down>\<^bsub>S\<^esub> \<Delta> : v"
+using assms
+proof(induction n arbitrary: \<Gamma> e S)
+  case 0
+  hence False by auto
+  thus ?case..
+next
+  case (Suc n)
+  show ?case
+  proof(cases e rule:exp_assn.strong_exhaust(1)[where c = "(\<Gamma>,S)", case_names Var App Let Lam])
+  case (Var x)
+    let ?e = "the (map_of \<Gamma> x)"
+    from Suc.prems[unfolded Var]
+    have "x \<in> heapVars \<Gamma>" by (auto intro: ccontr)
+    hence "(x, ?e) \<in> set \<Gamma>" by (induction \<Gamma>) auto
+    moreover
+    from Suc.prems[unfolded Var] `(x, ?e) \<in> set \<Gamma>` `x \<in> heapVars \<Gamma>`
+    have "(\<N>\<lbrakk>?e\<rbrakk>\<^bsub>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>" by auto
+    from Suc.IH[OF this]
+    obtain \<Delta> v where " \<Gamma> : ?e \<Down>\<^bsub>x # S\<^esub> \<Delta> : v" by blast
+    ultimately
+    have "\<Gamma> : (Var x) \<Down>\<^bsub>S\<^esub> (x,v) # delete x \<Delta> : v" by (rule VariableNoBH)
+    thus ?thesis using Var by auto
+  next
+  case (App e' x)
+
+    from Suc.prems[unfolded App]
+    have prem: "((\<N>\<lbrakk>e'\<rbrakk>\<^bsub>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<down>CFn C_case\<cdot>(\<N>\<lbrace>\<Gamma>\<rbrace> f!\<^sub>\<bottom> x))\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>" by (auto simp add: Rep_cfun_inverse)
+    hence e'_not_bot: "(\<N>\<lbrakk>e'\<rbrakk>\<^bsub>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>" by auto
+    from Suc.IH[OF this]
+    obtain \<Delta> v where lhs': "\<Gamma> : e' \<Down>\<^bsub>x#S\<^esub> \<Delta> : v" by blast 
+
+    from result_evaluated_fresh[OF lhs']
+    obtain y e'' where n': "v = (Lam [y]. e'')" and "atom y \<sharp> (\<Gamma>, e', x, S, \<Delta>)" by blast
+    with lhs'
+    have lhs: "\<Gamma> : e' \<Down>\<^bsub>x # S\<^esub> \<Delta> : Lam [y]. e''" by simp
+
+    from `atom y \<sharp> _` have "y \<notin> heapVars \<Delta>" by (metis (full_types) fresh_Pair heapVars_not_fresh)
+
+    have correct1: "\<N>\<lbrakk>e'\<rbrakk>\<^bsub>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub> \<sqsubseteq> \<N>\<lbrakk>Lam [y]. e''\<rbrakk>\<^bsub>\<N>\<lbrace>\<Delta>\<rbrace>\<^esub>" sorry
+    have correct2: "\<And> v. \<N>\<lbrace>\<Gamma>\<rbrace> f!\<^sub>\<bottom> v \<sqsubseteq> \<N>\<lbrace>\<Delta>\<rbrace> f!\<^sub>\<bottom> v" sorry
+
+    from e'_not_bot correct1
+    have lam_not_bot: "(\<N>\<lbrakk>Lam [y]. e''\<rbrakk>\<^bsub>\<N>\<lbrace>\<Delta>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>" by (metis below_bottom_iff monofun_cfun_fun)
+
+    have "((\<N>\<lbrakk>e'\<rbrakk>\<^bsub>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<down>CFn C_case\<cdot>(\<N>\<lbrace>\<Gamma>\<rbrace> f!\<^sub>\<bottom> x))\<cdot>C\<^bsup>n\<^esup>
+          \<sqsubseteq> ((\<N>\<lbrakk>Lam [y]. e''\<rbrakk>\<^bsub>\<N>\<lbrace>\<Delta>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<down>CFn C_case\<cdot>(\<N>\<lbrace>\<Gamma>\<rbrace> f!\<^sub>\<bottom> x))\<cdot>C\<^bsup>n\<^esup>"
+      by (intro monofun_cfun_arg monofun_cfun_fun correct1)
+    also have "\<dots> \<sqsubseteq> ((\<N>\<lbrakk>Lam [y]. e''\<rbrakk>\<^bsub>\<N>\<lbrace>\<Delta>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<down>CFn C_case\<cdot>(\<N>\<lbrace>\<Delta>\<rbrace> f!\<^sub>\<bottom> x))\<cdot>C\<^bsup>n\<^esup>"
+      by (intro monofun_cfun_arg monofun_cfun_fun correct2)
+    also have "\<dots> = (\<N>\<lbrakk>e''\<rbrakk>\<^bsub>(\<N>\<lbrace>\<Delta>\<rbrace>)(y f\<mapsto> C_case\<cdot>(\<N>\<lbrace>\<Delta>\<rbrace> f!\<^sub>\<bottom> x))\<^esub>)\<cdot>C\<^bsup>n\<^esup>"
+      using lam_not_bot `y \<notin> heapVars \<Delta>`
+      by (simp add: sharp_Env del: CESem.simps)
+    also have "\<dots> = (\<N>\<lbrakk>e''[y::=x]\<rbrakk>\<^bsub>\<N>\<lbrace>\<Delta>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup>" sorry
+    finally
+    have "\<dots> \<noteq> \<bottom>" using prem by auto
+    from Suc.IH[OF this]
+    obtain \<Theta> v' where rhs: "\<Delta> : e''[y::=x] \<Down>\<^bsub>S\<^esub> \<Theta> : v'" by blast
+    
+    have "\<Gamma> : App e' x \<Down>\<^bsub>S\<^esub> \<Theta> : v'"
+      by (rule reds_ApplicationI[OF `atom y \<sharp> _` lhs rhs])
+    thus ?thesis using App by auto
+  next
+  case (Lam v e')
+    have "\<Gamma> : Lam [v]. e' \<Down>\<^bsub>S\<^esub> \<Gamma> : Lam [v]. e'" ..
+    thus ?thesis using Lam by blast
+  next
+  case (Let as e')
+    have "distinctVars (asToHeap as)" sorry
+    moreover
+    {
+    from Suc.prems[unfolded Let] Let(1)
+    have prem: "(\<N>\<lbrakk>e'\<rbrakk>\<^bsub>\<N>\<lbrace>asToHeap as\<rbrace>\<N>\<lbrace>\<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>" 
+      by (simp add: Rep_cfun_inverse fresh_star_Pair) 
+    also have "\<N>\<lbrace>asToHeap as\<rbrace>\<N>\<lbrace>\<Gamma>\<rbrace> = \<N>\<lbrace>asToHeap as @ \<Gamma>\<rbrace>"
+      apply (rule HSem_merge)
+      sorry
+    finally 
+    have "(\<N>\<lbrakk>e'\<rbrakk>\<^bsub>\<N>\<lbrace>asToHeap as @ \<Gamma>\<rbrace>\<^esub>)\<cdot>C\<^bsup>n\<^esup> \<noteq> \<bottom>".
+    }
+    from Suc.IH[OF this]
+    obtain \<Delta> v where "asToHeap as @ \<Gamma> : e' \<Down>\<^bsub>S\<^esub> \<Delta> : v" by blast
+    ultimately
+    have "\<Gamma> : Let as e' \<Down>\<^bsub>S\<^esub> \<Delta> : v"
+      by (rule reds.Let[OF Let(1)])
+    thus ?thesis using Let by auto
+  qed
+qed
+
+end
+
