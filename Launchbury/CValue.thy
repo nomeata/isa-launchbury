c988c83f67a69d00468718219bd047668d53e680
Joachim Breitner <mail@joachim-breitner.de>
2013-10-10 17:28:54 +0000

    More about the C domain

--- Launchbury/CValue.thy
+++ Launchbury/CValue.thy
@@ -15,7 +15,25 @@ lemma below_C: "x \<sqsubseteq> C\<cdot>x"
 
 definition Cinf ("C\<^sup>\<infinity>") where "C\<^sup>\<infinity> = fix\<cdot>C"
 
+lemma C_Cinf[simp]: "C\<cdot>C\<^sup>\<infinity> = C\<^sup>\<infinity>" unfolding Cinf_def by (rule fix_eq[symmetric])
+
 abbreviation Cpow ("C\<^bsup>_\<^esup>") where "C\<^bsup>n\<^esup> \<equiv> iterate n\<cdot>C\<cdot>\<bottom>"
+
+lemma C_below_C[simp]: "(C\<^bsup>i\<^esup> \<sqsubseteq> C\<^bsup>j\<^esup>) \<longleftrightarrow> i \<le> j"
+  apply (induction i arbitrary: j)
+  apply simp
+  apply (case_tac j, auto)
+  done
+
+lemma below_Cinf[simp]: "r \<sqsubseteq> C\<^sup>\<infinity>"
+  apply (induct r)
+  apply simp_all[2]
+  apply (metis (full_types) C_Cinf monofun_cfun_arg)
+  done
+
+
+lemma C_eq_C[simp]: "(C\<^bsup>i\<^esup> = C\<^bsup>j\<^esup>) \<longleftrightarrow> i = j"
+  by (metis C_below_C le_antisym le_refl)
 
 lemma case_of_C_below: "(case r of C\<cdot>_ \<Rightarrow> x) \<sqsubseteq> x"
   by (cases r) auto
@@ -26,6 +44,71 @@ lemma C_case_below: "C_case \<cdot> f \<sqsubseteq> f"
 lemma C_case_Cinf[simp]: "C_case \<cdot> f \<cdot> C\<^sup>\<infinity> = f \<cdot> C\<^sup>\<infinity>"
   unfolding Cinf_def
   by (subst fix_eq) simp
+
+lemma nat_mono_characterization:
+  fixes f :: "nat \<Rightarrow> nat"
+  assumes "mono f"
+  obtains n where "\<And>m . n \<le> m \<Longrightarrow> f n = f m" | "\<And> m . \<exists> n . m \<le> f n"
+proof (cases "finite (range f)")
+  case True
+  from Max_in[OF True]
+  obtain n where Max: "f n = Max (range f)" by auto
+  show thesis
+  proof(rule that)
+    fix m
+    assume "n \<le> m"
+    hence "f n \<le> f m" using `mono f` by (metis monoD)
+    also
+    have "f m \<le> f n" unfolding Max by (rule Max_ge[OF True rangeI])
+    finally
+    show "f n = f m".
+  qed
+next
+  case False
+  thus thesis by (fastforce intro: that(2) simp add: infinite_nat_iff_unbounded_le)
+qed
+
+lemma C_cases:
+  obtains n where "r = C\<^bsup>n\<^esup>" | "r = C\<^sup>\<infinity>"
+proof-
+  { fix m
+    have "\<exists> n. C_take m \<cdot> r = C\<^bsup>n\<^esup> "
+    proof (rule C.finite_induct)
+      have "\<bottom> = C\<^bsup>0\<^esup>" by simp
+      thus "\<exists>n. \<bottom> = C\<^bsup>n\<^esup>"..
+    next
+      fix r
+      show "\<exists>n. r = C\<^bsup>n\<^esup> \<Longrightarrow> \<exists>n. C\<cdot>r = C\<^bsup>n\<^esup>"
+        by (auto simp del: iterate_Suc simp add: iterate_Suc[symmetric])
+    qed
+  }
+  then obtain f where take: "\<And> m. C_take m \<cdot> r = C\<^bsup>f m\<^esup>" by metis
+  have "chain (\<lambda> m. C\<^bsup>f m\<^esup>)" using ch2ch_Rep_cfunL[OF C.chain_take, where x=r, unfolded take].
+  hence "mono f" by (auto simp add: mono_iff_le_Suc chain_def elim!:chainE)
+  have r: "r = (\<Squnion> m. C\<^bsup>f m\<^esup>)"  by (metis (lifting) take C.reach lub_eq)
+  from `mono f`
+  show thesis
+  proof(rule nat_mono_characterization)
+    fix n
+    assume n: "\<And> m. n \<le> m ==> f n = f m"
+    have "max_in_chain n (\<lambda> m. C\<^bsup>f m\<^esup>)"
+      apply (rule max_in_chainI)
+      apply simp
+      apply (erule n)
+      done
+    hence "(\<Squnion> m. C\<^bsup>f m\<^esup>) = C\<^bsup>f n\<^esup>" unfolding  maxinch_is_thelub[OF `chain _`].
+    thus ?thesis using that unfolding r by blast
+  next
+    assume "\<And>m. \<exists>n. m \<le> f n"
+    hence "\<And> n. C\<^bsup>n\<^esup> \<sqsubseteq> r" unfolding r by (fastforce intro: below_lub[OF `chain _`])
+    hence "(\<Squnion> n. C\<^bsup>n\<^esup>) \<sqsubseteq> r" 
+      by (rule lub_below[OF chain_iterate])
+    hence "C\<^sup>\<infinity> \<sqsubseteq> r" unfolding Cinf_def fix_def2.
+    hence "C\<^sup>\<infinity> = r" using below_Cinf by (metis below_antisym)
+    thus thesis using that by blast
+  qed
+qed
+
 
 instantiation C :: Finite_Meet_cpo begin
   fixrec C_meet :: "C \<rightarrow> C \<rightarrow> C"
