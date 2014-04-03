702871e5615eb960408cb78bb58c46d5edbeeb86
Joachim Breitner <mail@joachim-breitner.de>
2014-04-03 09:23:19 +0000

    Remove unused theorems

--- Launchbury/ArityCorrect.thy
+++ Launchbury/ArityCorrect.thy
@@ -58,38 +58,6 @@ next
     by simp
 qed  
 
-(* http://afp.sourceforge.net/browser_info/current/AFP/WorkerWrapper/FixedPointTheorems.html *)
-lemma rolling_rule_ltr: "fix\<cdot>(g oo f) \<sqsubseteq> g\<cdot>(fix\<cdot>(f oo g))"
-proof -
-  have "g\<cdot>(fix\<cdot>(f oo g)) \<sqsubseteq> g\<cdot>(fix\<cdot>(f oo g))"
-    by (rule below_refl) -- {* reflexivity *}
-  hence "g\<cdot>((f oo g)\<cdot>(fix\<cdot>(f oo g))) \<sqsubseteq> g\<cdot>(fix\<cdot>(f oo g))"
-    using fix_eq[where F="f oo g"] by simp -- {* computation *}
-  hence "(g oo f)\<cdot>(g\<cdot>(fix\<cdot>(f oo g))) \<sqsubseteq> g\<cdot>(fix\<cdot>(f oo g))"
-    by simp -- {* re-associate @{term "op oo"} *}
-  thus "fix\<cdot>(g oo f) \<sqsubseteq> g\<cdot>(fix\<cdot>(f oo g))"
-    using fix_least_below by blast -- {* induction *}
-qed
-
-lemma rolling_rule_rtl: "g\<cdot>(fix\<cdot>(f oo g)) \<sqsubseteq> fix\<cdot>(g oo f)"
-proof -
-  have "fix\<cdot>(f oo g) \<sqsubseteq> f\<cdot>(fix\<cdot>(g oo f))" by (rule rolling_rule_ltr)
-  hence "g\<cdot>(fix\<cdot>(f oo g)) \<sqsubseteq> g\<cdot>(f\<cdot>(fix\<cdot>(g oo f)))"
-    by (rule monofun_cfun_arg) -- {* g is monotonic *}
-  thus "g\<cdot>(fix\<cdot>(f oo g)) \<sqsubseteq> fix\<cdot>(g oo f)"
-    using fix_eq[where F="g oo f"] by simp -- {* computation *}
-qed
-
-lemma rolling_rule: "fix\<cdot>(g oo f) = g\<cdot>(fix\<cdot>(f oo g))"
-  by (rule below_antisym[OF rolling_rule_ltr rolling_rule_rtl])
-
-lemma rolling_rule2:
-  assumes "cont F" and "cont G"
-  shows "fix\<cdot>(\<Lambda> x. G (F x)) = G (fix\<cdot>(\<Lambda> x. F (G x)))"
-  using cont_compose[OF assms(1), cont2cont] cont_compose[OF assms(2), cont2cont]
-  using rolling_rule[where g = "\<Lambda> x. G x" and f = "\<Lambda> x. F x"]
-  by (auto simp add: oo_def)
-
 lemma Afix_join_fresh: "ae' ` (domA \<Delta>) \<subseteq> {\<bottom>}  \<Longrightarrow>  Afix \<Delta>\<cdot>(ae \<squnion> ae') = (Afix \<Delta>\<cdot>ae) \<squnion> ae'"
   apply (rule below_antisym)
   apply (rule Afix_least_below)
@@ -137,21 +105,6 @@ next
   show "ABinds (((v, e) # \<Delta>) @ \<Gamma>)\<cdot>ae = ABinds ((v, e) # \<Delta>)\<cdot>(ABinds \<Gamma>\<cdot>ae)"
     by simp
 qed  
-
-lemma Abinds_comp2join_fresh:
-  "atom ` (domA \<Delta>) \<sharp>* \<Gamma> \<Longrightarrow> ABinds \<Delta>\<cdot>(ABinds \<Gamma>\<cdot>ae) = ABinds \<Gamma> \<cdot> ae \<squnion> ABinds \<Delta>\<cdot>ae"
-proof (induct \<Delta> rule: ABinds.induct)
-  case 1 show ?case by (simp add: ABinds_above_arg del: fun_meet_simp)
-next
-  case (2 v e \<Delta>)
-  from 2(2)
-  have "atom v \<sharp> \<Gamma>" and  "atom ` domA (delete v \<Delta>) \<sharp>* \<Gamma>"
-    by (auto simp add: fresh_star_def)
-  from 2(1)[OF this(2)]
-  show ?case
-    by (simp del: fun_meet_simp add: ABinds_lookup_fresh[OF `atom v \<sharp> \<Gamma>`])
-       (metis (hide_lams, no_types) join_assoc join_comm)
-qed
 
 lemma Afix_comp2join_fresh:
   "atom ` (domA \<Delta>) \<sharp>* \<Gamma> \<Longrightarrow> ABinds \<Delta>\<cdot>(Afix \<Gamma>\<cdot>ae) = Afix \<Gamma> \<cdot> ae \<squnion> ABinds \<Delta>\<cdot>ae"
@@ -330,7 +283,5 @@ proof-
   show ?thesis by (rule reds_improves_arity'[where ae = \<bottom>, OF assms, simplified]) auto
 qed
 end
-
-unused_thms
 
 end
