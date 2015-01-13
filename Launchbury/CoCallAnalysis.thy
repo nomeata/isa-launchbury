a2f8bb7d9eef91f69122e6dad0d2d61126d76616
Joachim Breitner <mail@joachim-breitner.de>
2015-01-13 10:34:08 +0000

    Unused locale

--- Launchbury/CoCallAnalysis.thy
+++ Launchbury/CoCallAnalysis.thy
@@ -31,13 +31,4 @@ locale CoCallAnalysis_ccField = CoCallAnalysis +
 locale CoCallAnalyisHeap = 
   fixes ccHeap :: "heap \<Rightarrow> exp \<Rightarrow> Arity \<rightarrow> CoCalls"
 
-locale CorrectCoCallAnalysis = CoCallAnalysis_ccField +
-  assumes Aexp_eqvt[eqvt]: "\<pi> \<bullet> ccExp = ccExp"
-  (* assumes Aexp_Var: "up \<cdot> n \<sqsubseteq> (ccExp (Var x) \<cdot> n) x" *) 
-  (* assumes Aexp_subst_App_Lam: "ccExp (e[y::=x]) \<sqsubseteq> ccExp (App (Lam [y]. e) x)" *)
-  assumes Aexp_Lam0: "ccExp (Lam [y]. e) \<cdot> 0 = cc_delete y (ccSquare (fv e))"
-  assumes Aexp_Lam1: "ccExp (Lam [y]. e) \<cdot> (inc\<cdot>n) = cc_delete y (ccExp e \<cdot>n)"
-  assumes Aexp_App: "ccExp (App e x)\<cdot>n = ccExp e \<cdot>(inc\<cdot>n) \<squnion> ccProd (fv e) {x}"
-  (* assumes Aexp_subst_restr: "x \<notin> S \<Longrightarrow> y \<notin> S \<Longrightarrow> (ccExp e[x::=y] \<cdot> a) f|` S = (ccExp e\<cdot>a) f|` S" *)
-
 end
